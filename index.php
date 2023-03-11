68eb0e048f2f21702c551a41ac727ed7d6861385
igor.znamensky <igor.znamensky@gmail.com>
2023-03-11 12:58:22 +0300

    6, 10:51

---
+++ index.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('Главная');
+?>
+    <p>Это моя страница в 1С Bitrix</p>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
